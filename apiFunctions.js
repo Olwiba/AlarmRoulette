6a3f8150cec28524ab9a24b65caf6f6ebf2edaa3
Marcus Crane <marcus@thingsima.de>
2016-07-15 14:59:53 +1200

    Added Tumblr functionality

--- apiFunctions.js
+++ apiFunctions.js
@@ -1,14 +1,22 @@
 var request = require('superagent')
 var env = require('dotenv')
 var Twitter = require('twitter')
+var Tumblr = require('tumblr.js')
 
 env.load()
 
-var client = new Twitter({
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token_key: process.env.ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+var twitter = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+})
+
+var client = Tumblr.createClient({
+  consumer_key: process.env.TUMBLR_CONSUMER_KEY,
+  consumer_secret: process.env.TUMBLR_CONSUMER_SECRET,
+  token: process.env.TUMBLR_TOKEN,
+  token_secret: process.env.TUMBLR_TOKEN_SECRET
 })
 
 function postToSlack(message) {
@@ -27,7 +35,7 @@ function postToSlack(message) {
 }
 
 function postToTwitter(message) {
-  client.post('statuses/update', { status: message }, function(err, tweets, res){
+  twitter.post('statuses/update', { status: message }, function(err, tweets, res){
     if (err) {
       console.log("Twitter Error: ", err)
     } else {
@@ -37,10 +45,36 @@ function postToTwitter(message) {
   })
 }
 
-var number = Math.floor(Math.random() * (1 - 100)) + 100
+// function postToTumblr(message) {
+//   Tumblr(message, 'genericstuff', tumblr_auth, function(error, response) {
+//     console.log("Hit Tumblr")
+//   })
+// }
 
-postToTwitter('@sentreh ' + number)
-if (number < 50) {
+function postToTumblr(message) {
+  client.createTextPost('genericstuff', {type: 'text', title: 'Sleepyhead', body: message }, function(err, res) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('Hit Tumblr')
+    }
+  })
+}
+
+var number = Math.floor(Math.random() * (1 - 99)) + 99
+
+
+function post(api) {
+  console.log('Rolled ', number, ' and hit ', api)
+}
+
+if (number > 66) {
+  // postToTwitter('Test: RNG was ' + number)
+  post('twitter')
+} else if (number < 66 && number > 33) {
+  // postToTumblr('Test: RNG was ' + number)
+  post('tumblr')
 } else {
-  postToSlack(number)
+  // postToSlack('Test: RNG was ' + number)
+  post('slack')
 }
